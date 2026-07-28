aaa06d80b612b71d0750073533d6f5dd1967b6c4
hassanmansoori37 <mansoorihassan409@gmail.com>
2026-07-29 01:36:34 +0500

    crud operation completed

--- routes/posts/index.mjs
+++ routes/posts/index.mjs
@@ -1,4 +1,5 @@
 import express from 'express'
+import { title } from 'node:process';
 
 const router = express.Router()
 
@@ -46,15 +47,106 @@ router.get('/post' , (req, res) => {
 })
 
 router.get('/post/:postId' , (req, res) => {
-    res.send("post get single")
+    const postId = req.params.postId
+    if(!postId){
+        return res.status(400).send({
+            message: "post id is required"
+        })
+        
+    }
+
+    const post = allPosts.find((singlePost) => {
+        return singlePost.id == postId
+    })
+
+   if(!post){
+        return res.status(404).send({
+             message: "page not found"
+        })
+       
+    }
+    res.send({
+        message: "post fetched",
+        data: post,
+    })
 })
 
 router.put('/post/:postId' , (req, res) => {
-    res.send("post edited")
+     const postId = req.params.postId
+
+      if(!req.body.title){
+        res.status(400).send({
+            message: "title is required"
+        })
+    }
+
+     if(!req.body.description){
+        res.status(400).send({
+            message: "description is required"
+        })
+    }
+
+
+    if(!postId){
+        return res.status(404).send({
+            message: "post id is required"
+        })
+        
+    }
+
+    const post = allPosts.find((singlePost) => {
+        return singlePost.id == postId
+    })
+
+    if(!post){
+        return res.status(404).send({
+             message: "page not found"
+        })
+       
+    }
+
+    const newPosts = allPosts.map((singlePost) => {
+        return singlePost.id == postId ? {
+            ...singlePost,
+            title: req.body.title,
+            description: req.body.description,
+        }
+        : singlePost
+    })
+
+    allPosts = newPosts
+    res.send({
+        message: "post edit"
+    })
 })
 
 router.delete('/post/:postId' , (req, res) => {
-    res.send("post deleted")
+    const postId = req.params.postId
+    if(!postId){
+        return res.status(404).send({
+            message: "post id is required"
+        })
+        
+    }
+
+    const post = allPosts.find((singlePost) => {
+        return singlePost.id == postId
+    })
+
+    if(!post){
+        return res.status(404).send({
+             message: "page not found"
+        })
+       
+    }
+     const newPosts = allPosts.filter((singlePost) => {
+        return singlePost.id != postId
+     })
+
+     allPosts = newPosts
+    res.send({
+        message: "post deleted"
+    })
 })
 
 export default router
